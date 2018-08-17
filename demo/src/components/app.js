1d4209ff7573db81b219108b90a3bf1d63604e1f
mhmtztmr <mahmutoztemur@gmail.com>
2018-08-17 19:48:33 +0300

    two-sided demo added

--- demo/src/components/app.js
+++ demo/src/components/app.js
@@ -1,25 +1,132 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Dialpad } from '../../../dist/bundle';
 
-class App extends Component {
+class App extends React.Component {
 	constructor() {
 		super();
-		this.state = {
-			call: {}
-		};
-		this.startCall = this.startCall.bind(this);
+
+		this.onActionInvoked = this.onActionInvoked.bind(this);
+		this.onStateChanged = this.onStateChanged.bind(this);
+		this.onCallStarted = this.onCallStarted.bind(this);
+		this.onCallEnded = this.onCallEnded.bind(this);
+		this.onCallReplied = this.onCallReplied.bind(this);
+		this.onCallRejected = this.onCallRejected.bind(this);
+
+		this.onRemoteActionInvoked = this.onRemoteActionInvoked.bind(this);
+		this.onRemoteCallStarted = this.onRemoteCallStarted.bind(this);
+		this.onRemoteCallEnded = this.onRemoteCallEnded.bind(this);
+		this.onRemoteCallReplied = this.onRemoteCallReplied.bind(this);
+		this.onRemoteCallRejected = this.onRemoteCallRejected.bind(this);
 	}
 
-	startCall(call) {
-		this.setState({
-			call: call
-		});
+	/*  DIALPAD */
+	onActionInvoked(action, data) {
+		switch (action) {
+			case 'CALL_STARTED':
+				this.onCallStarted(data.number);
+				break;
+			case 'CALL_ENDED':
+				this.onCallEnded();
+				break;
+			case 'CALL_REPLIED':
+				this.onCallReplied();
+				break;
+			case 'CALL_REJECTED':
+				this.onCallRejected();
+				break;
+			default:
+				break;
+		}
+	}
+
+	onStateChanged(state) {
+		console.log('stateee', state);
+	}
+
+	onCallStarted(number) {
+		console.log('Callling...', number);
 		setTimeout(() => {
-			this.state.call.onRinging();
-		}, 5000);
+			this.dialpad.setRinging();
+
+			this.remoteDialpad.setIncomingCall({
+				name: 'You',
+				number: '+1234567890'
+			});
+		}, 3000);
+	}
+
+	onCallEnded() {
 		setTimeout(() => {
-			this.state.call.onCall();
-		}, 10000);
+			this.dialpad.endCall();
+			this.remoteDialpad.endCall();
+		}, 1000);
+	}
+
+	onCallReplied(event) {
+		setTimeout(() => {
+			this.dialpad.setOnCall();
+			this.remoteDialpad.setOnCall();
+		}, 1000);
+	}
+
+	onCallRejected(event) {
+		setTimeout(() => {
+			this.dialpad.endCall();
+			this.remoteDialpad.endCall();
+		}, 1000);
+	}
+
+	/* REMOTE DIALPAD */
+
+	onRemoteActionInvoked(action, data) {
+		switch (action) {
+			case 'CALL_STARTED':
+				this.onRemoteCallStarted(data.number);
+				break;
+			case 'CALL_ENDED':
+				this.onRemoteCallEnded();
+				break;
+			case 'CALL_REPLIED':
+				this.onRemoteCallReplied();
+				break;
+			case 'CALL_REJECTED':
+				this.onRemoteCallRejected();
+				break;
+			default:
+				break;
+		}
+	}
+
+	onRemoteCallStarted(number, event) {
+		console.log('Callling...', number);
+		setTimeout(() => {
+			this.remoteDialpad.setRinging();
+			this.dialpad.setIncomingCall({
+				name: 'Remote',
+				number: '+9012345678'
+			});
+		}, 3000);
+	}
+
+	onRemoteCallEnded(event) {
+		setTimeout(() => {
+			this.dialpad.endCall();
+			this.remoteDialpad.endCall();
+		}, 1000);
+	}
+
+	onRemoteCallReplied(event) {
+		setTimeout(() => {
+			this.dialpad.setOnCall();
+			this.remoteDialpad.setOnCall();
+		}, 1000);
+	}
+
+	onRemoteCallRejected(event) {
+		setTimeout(() => {
+			this.dialpad.endCall();
+			this.remoteDialpad.endCall();
+		}, 1000);
 	}
 
 	render() {
@@ -30,9 +137,18 @@ class App extends Component {
 
 		return (
 			<div className="app">
+				<div>You (Local): +1234567890</div>
 				<Dialpad
 					style={style}
-					onCallPressed={this.startCall}
+					ref={dialpad => (this.dialpad = dialpad)}
+					onActionInvoked={this.onActionInvoked}
+					onStateChanged={this.onStateChanged}
+				/>
+				<div>Remote: +9012345678</div>
+				<Dialpad
+					style={style}
+					ref={dialpad => (this.remoteDialpad = dialpad)}
+					onActionInvoked={this.onRemoteActionInvoked}
 				/>
 			</div>
 		);
