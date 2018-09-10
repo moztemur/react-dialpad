5fde5d688d0195e344acf2850fc0e2087c4ef1eb
mhmtztmr <mahmutoztemur@gmail.com>
2018-09-11 00:05:12 +0300

    ut for base button using jest and enzyme

--- test/components/dialpad/_shared/button.spec.js
+++ test/components/dialpad/_shared/button.spec.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import Button from '../../../src/components/dialpad/_shared/button';
-import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+import Button from '../../../../src/components/dialpad/_shared/button';
 
-test('Button', () => {
-  const component = renderer.create(
-    <Button className="digit" value={2} subvalue={'abc'} />,
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+test('Button without subvalue to have 1 divs', () => {
+  const wrapper = shallow(<Button className="digit" value={1} />);
+  expect(wrapper.find('div').length).toBe(1);
+});
+
+test('Button with subvalue to have 2 divs', () => {
+  const wrapper = shallow(<Button className="digit" value={2} subvalue={'abc'} />);
+  expect(wrapper.find('div').length).toBe(2);
+});
+
+test('Button with subvalue 2 to have call onPress with 2', () => {
+  const onPressed = jest.spyOn(Button.prototype, 'onPressed');
+  const mck = jest.fn();
+  const wrapper = shallow(<Button className="digit" value={2} subvalue={'abc'} onPressed={mck} />);
+  wrapper.find('div').first().prop('onClick')();
+  expect(onPressed).toHaveBeenCalledWith(2);
+  expect(mck).toHaveBeenCalled();
+});
+
+
+test('Button with subvalue 2 to have call onPress with 2', () => {
+  const onPressed = jest.fn();
+  const wrapper = shallow(<Button className="digit" value={2} subvalue={'abc'} onPressed={onPressed} />);
+  wrapper.find('div').first().prop('onClick')();
+  expect(onPressed).toHaveBeenCalledWith(2);
+});
+
+test('Button with subvalue 2 and enabler states excluding call state does not call onPressed', () => {
+  const onPressed = jest.fn();
+  const wrapper = shallow(<Button className="digit" value={2} subvalue={'abc'} onPressed={onPressed} enablerStates={['ON_CALL']} callState={'RINGING'} />);
+  wrapper.find('div').first().prop('onClick')();
+  expect(onPressed).not.toHaveBeenCalled();
 });
