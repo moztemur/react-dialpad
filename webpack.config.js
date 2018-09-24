35d306e43712a72c32d0c172228174b279bb9bbb
mhmtztmr <mahmutoztemur@gmail.com>
2018-09-24 21:25:06 +0300

    babel plugin for folder import

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -31,11 +30,6 @@ module.exports = {
         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
         use: 'url-loader?limit=1500000&img?progressive=true',
       },
-    ]
-  },
-  resolve: {
-    plugins: [
-      new DirectoryNamedWebpackPlugin(true)
     ]
   }
 };
